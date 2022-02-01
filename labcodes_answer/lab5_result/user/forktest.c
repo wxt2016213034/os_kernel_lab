c7fc217624e9ada5973b4ac9f757913676a6415c
王旭韬 <wangxutao@wangxutaodeMacBook-Pro.local>
2022-02-01 17:17:41 +0000

    feat: test

--- labcodes_answer/lab5_result/user/forktest.c
+++ labcodes_answer/lab5_result/user/forktest.c
@@ -3,30 +3,40 @@
 
 const int max_child = 32;
 
-int
-main(void) {
-    int n, pid;
-    for (n = 0; n < max_child; n ++) {
+void fk(){
+    int n = 2;
+    int pid;
         if ((pid = fork()) == 0) {
             cprintf("I am child %d\n", n);
             exit(0);
         }
-        assert(pid > 0);
-    }
+}
 
-    if (n > max_child) {
-        panic("fork claimed to work %d times!\n", n);
-    }
+int
+main(void) {
+    int n, pid;
+    fk();
+    // for (n = 0; n < max_child; n ++) {
+    //     if ((pid = fork()) == 0) {
+    //         cprintf("I am child %d\n", n);
+    //         exit(0);
+    //     }
+    //     assert(pid > 0);
+    // }
 
-    for (; n > 0; n --) {
-        if (wait() != 0) {
-            panic("wait stopped early\n");
-        }
-    }
+    // if (n > max_child) {
+    //     panic("fork claimed to work %d times!\n", n);
+    // }
 
-    if (wait() == 0) {
-        panic("wait got too many\n");
-    }
+    // for (; n > 0; n --) {
+    //     if (wait() != 0) {
+    //         panic("wait stopped early\n");
+    //     }
+    // }
+
+    // if (wait() == 0) {
+    //     panic("wait got too many\n");
+    // }
 
     cprintf("forktest pass.\n");
     return 0;
